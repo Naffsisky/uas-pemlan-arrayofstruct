cf9aeb61eab319366fa54d2bfa961649c3d53deb
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-12-06 00:08:09 +0700

    Update uas.c

--- uas.c
+++ uas.c
@@ -23,6 +23,7 @@ int menu(){
 
 int main(){
 
+    FILE *fp;
 	struct data dosen[100];
 	int i, j, banyak, total, pilih, id;
 	
@@ -53,17 +54,15 @@ int main(){
 			break;
 			
 			case 2:
-                system("clear");
-                printf("\nData Dosen : \n");
                 if (total == 0){
                     printf("Data masih kosong!\nSilahkan tambahkan data terlebih dahulu!\n\n");
                 }
                 else{
                     for (i = 0; i < total; i++) {
-					printf("Data ke-%d\n", i+1);
-        			printf("ID : %d\n", dosen[i].id);
-        			printf("Nama : %s\n", dosen[i].nama);
-        			printf("Prodi : %s\n\n", dosen[i].prodi);
+                        printf("Data ke-%d\n", i+1);
+                        printf("ID : %d\n", dosen[i].id);
+                        printf("Nama : %s\n", dosen[i].nama);
+                        printf("Prodi : %s\n\n", dosen[i].prodi);
 				    }
                 }
             break;
@@ -88,5 +87,13 @@ int main(){
 	} while (pilih != 0);
     system("clear");
 	printf("Terima kasih!\n");
+    fp = fopen("jajal.txt", "w+");
+    for (i = 0; i < total; i++) {
+        fprintf(fp,"Data ke-%d\n", i+1);
+        fprintf(fp,"ID : %d\n", dosen[i].id);
+        fprintf(fp,"Nama : %s\n", dosen[i].nama);
+        fprintf(fp,"Prodi : %s\n\n", dosen[i].prodi);
+    }
+    fclose(fp);
 	return 0;
 }
